8a2e82773c7e8b4b5ac1da0781d8085cb8b74f17
Dean R <platoisalive@outlook.com>
2023-06-12 04:20:28 +0000

    added controls box

--- src/components/ui/coder/Coder.tsx
+++ src/components/ui/coder/Coder.tsx
@@ -3,11 +3,13 @@ import "./coder.css";
 import React from "react";
 import Instructions from "./instructions/Instructions";
 import Memory from "./memory/Memory";
+import Controls from "./controls/Controls";
 
 const Coder = () => {
 	return (
 		<div className="coder">
 			<Instructions />
+			<Controls />
 			<Memory />
 		</div>
 	);
