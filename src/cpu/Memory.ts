0179ba44af592b9e8533711b68ba9888d5100b77
Dean R <platoisalive@outlook.com>
2023-07-11 13:27:03 +0000

    added more tests

--- src/cpu/Memory.ts
+++ src/cpu/Memory.ts
@@ -43,16 +43,20 @@ export const memoryTest = () => {
 		memory.getAll().every((v) => v === 0),
 		"get all, all are zero"
 	);
-	console.log(memory.getAt(-1) === null, "get bounds check -1");
-	console.log(memory.getAt(1) === 0, "get at 1 is 0");
+	console.log(memory.getAt(-1) === null, "get bounds check [-1]");
+	console.log(memory.getAt(1) === 0, "get at [1] == 0");
 	console.log(memory.getAt(8) === null, "get bounds check > length");
-	console.log(!memory.setAt(-1, 1), "set bounds check -1");
-	console.log(memory.setAt(1, 1) && memory.getAt(1) === 1, "set at 1 is 1");
+	console.log(!memory.setAt(-1, 1), "set bounds check [-1]");
+	console.log(memory.setAt(1, 1) && memory.getAt(1) === 1, "set at [1] == 1");
 	console.log(!memory.setAt(8, 1), "set bounds check > length");
+
+	memory.setAt(1, 1);
+	memory.setAt(0, 1);
+	console.log(memory.getAtIndirect(0) === 1, "get indirect [0]->[1] == 1");
 
 	memory.setAt(1, 0);
 	console.log(
 		memory.setAtIndirect(1, 1) && memory.getAt(0) === 1,
-		"set indirect 2 (0) to 1, 0 is 1"
+		"set indirect [1]->[0] to 1, [0] == 6"
 	);
 };
